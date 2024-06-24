5d3e49769482b668dba632b648bdb7ef572ddaa7
Sujan <sujansahu655@gmail.com>
2024-06-24 22:36:18 +0530

    comments and next line

---
+++ DAY-01/basics1.py
@@ -0,0 +1,19 @@
+print("Hy im good boy \n And loks good")  # /n go to next line 
+# its coment declartion ( ctrl+/ )
+print("This is print statement")
+print("Hellow !!") # code under its any deccription its used for coment
+
+#Multi line Comments
+
+"""
+ Hey Sujan Pless dont forget me
+ crete by sujan 
+"""
+# Escape Sequence Charecter
+print("Hey I am \"good boy \"\n and this viwewr is also good boy") # foreward slase '\' are used show for Escapes Sequence
+
+#print statement 
+print("Hey",5,6,4.6,sep="$",end="34\n") #used for ,  to add multiple data in one print stae ment
+# sep arr add to all are saparet
+#end print the last 
+ 
