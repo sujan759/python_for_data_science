fff80b3c8eb328722351e374a158714300192cb4
Sujan <sujansahu655@gmail.com>
2024-06-24 19:37:51 +0530

    basics of python

---
+++ DAY-01/basics.py
@@ -0,0 +1,4 @@
+print("Hello world") #print string data type " under any thing data"
+print(2)  #print int,folt, value direct
+print(2+8)  # print math opertion same data type input 
+print("Hy im good boy \n And loks good")  #
