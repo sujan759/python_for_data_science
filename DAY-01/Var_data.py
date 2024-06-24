d6737430d23de0f3354b948258b1221058a1c264
Sujan <sujansahu655@gmail.com>
2024-06-24 22:36:45 +0530

    about variables and data types

---
+++ DAY-01/Var_data.py
@@ -0,0 +1,30 @@
+a = 190
+print(a)
+b ="sujan"
+print(b)
+# variable is like container to hold data
+
+        #  data type
+a=3
+b= True
+c=None
+d="sujan"
+e=43.43
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+# 1, Numaric data: int, flot, compelx
+# 2, Text data: str
+# 3, Boolean data: True,false
+# 4, Sequenced data: list,tuple 
+list=[4,3,3,[-34,34,33],['appel','mango','banana']]
+print(list)
+tuple=(('parrot','sparrow'),("Lion","Tiger"))
+print (tuple)
+# 5, Mapped data :  dict
+dict={"name":"Sujan", "age":21,"canVote":True}
+print(dict)
+ 
+
