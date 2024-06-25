35908baed354ae42ee2a0e27ca89dcd04ee922b0
Sujan <sujansahu655@gmail.com>
2024-06-25 22:01:12 +0530

    Type_casting

---
+++ DAY-2/Typecasting.py
@@ -0,0 +1,20 @@
+#  the conversion of one data type to other data type casting or conversion python 
+#  python suport wide varit of function or method : int(), float(),str(),ord(),hex(),oct(),tuple(),list(), dict(), etc . type casting in python
+#  Two Types of Type casting 
+# 1.Explicit Conversion
+
+a="2"
+
+b="5"
+print(int(a)+ int(b)) # valid data type input you declar the data
+ 
+# 2.Implicit Conversion
+ 
+c=77.0
+print(type(c))
+
+d=8
+print(type(d))
+
+print(c+d)
+#  python  auto matic detect 
