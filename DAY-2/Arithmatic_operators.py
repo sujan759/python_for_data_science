c2dc74756b645382f31f1f946de26d876f3c46be
Sujan <sujansahu655@gmail.com>
2024-06-25 21:58:48 +0530

    Arithmetic

---
+++ DAY-2/Arithmatic_operators.py
@@ -0,0 +1,10 @@
+a=12
+b=5
+print("Addition operation",a+b)
+print("Subtraction operation",a-b)
+print("Multiplication operation",a*b)
+print("Divison operation",a/b)
+print("Floor Divison operation",a//b) # Fractional vall 
+print("Modulus operation",a%b)# print remander
+print("expontiol operation",a**b);
+
