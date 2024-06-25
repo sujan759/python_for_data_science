f80076ebae492ae808c68453804cdf993bf41243
Sujan <sujansahu655@gmail.com>
2024-06-25 22:00:24 +0530

    Calculator

---
+++ DAY-2/Calculator.py
@@ -0,0 +1,10 @@
+# input  used to user input data  
+a=int(input("Enter first number"))
+b=int(input("Enter second number"))
+print("Addition operation",a+b)
+print("Subtraction operation",a-b)
+print("Multiplication operation",a*b)
+print("Divison operation",a/b)
+print("Floor Divison operation",a//b) # Fractional valu not consider  
+print("Modulus operation",a%b)# print remander
+print("expontiol operation",a**b)
