f9eeed75b3d3118f2cd84d05ff4d1453f5eda66c
sujan759 <153996083+sujan759@users.noreply.github.com>
2024-06-26 20:33:18 +0530

    Delete DAY-3 directory

--- DAY-3/String_Slicing.py
+++
@@ -1,7 +0,0 @@
-name= "sujan , rahul"
-print(len(name)) # find length of string 
-# Slicing 
-print(name[0:6]) #
-n="harya"
-print(n[-4:-2])
-print(34)
--- DAY-3/Userinput.py
+++
@@ -1,8 +0,0 @@
-
-#  Syntecs
-variable = input()
-b=input("Enter your Name:")
-print(b)
-a=input("Enter first number")
-b=input("Enter second number")
-print(a+b) # defult it consider str 
