52b08831b070d00ecb79d637dfe646d4e2f32069
Sujan <sujansahu655@gmail.com>
2024-06-28 09:31:27 +0530

    For_lops

---
+++ DAY-5/For_Lops.py
@@ -0,0 +1,12 @@
+# For used for string
+name='sujan'
+for i in name:
+    print(i)
+    if(i=='j'):
+        print('Special')
+color=['Red','Blue','White']
+for y in color:
+    print(y)
+# range used
+for k in range(10,23):
+    print(k) 
