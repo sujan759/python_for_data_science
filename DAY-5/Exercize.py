dad2e3eb9a1541c64f65dc064d7ccc30d597d708
Sujan <sujansahu655@gmail.com>
2024-06-28 09:31:46 +0530

    pratice2

--- DAY-5/Exercize.py
+++ DAY-5/Exercize.py
@@ -12,6 +12,6 @@ if timestamp>="12:00:00" and  timestamp<"16:00:00" :
     print("AfterNon")
 if timestamp>"16:00:00" and  timestamp<"19:00:00" :
     print("evening")
-if timestamp1>"19" and  timestamp1<"23" and timestamp2<'59' :
+if timestamp1>"19" and  timestamp1<"23" and timestamp2<'' :
     print("good night")
     
